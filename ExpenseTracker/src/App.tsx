f345bb0e0951c4559bf162193944180d88cdb934
srt1017 <omarfarouq107@gmail.com>
2023-05-18 19:10:26 -0500

    Initialised project. Built expense list component using statehook and simple table layout.

---
+++ ExpenseTracker/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+}
+
+export default App;
