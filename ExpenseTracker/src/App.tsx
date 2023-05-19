20847e550d400dafc3149cdf4817b60df7ae23de
srt1017 <omarfarouq107@gmail.com>
2023-05-18 19:40:27 -0500

    Added expense filter component that allows user to filter list by expense category

--- ExpenseTracker/src/App.tsx
+++ ExpenseTracker/src/App.tsx
@@ -3,22 +3,34 @@ import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Entertainment" },
     { id: 3, description: "ccc", amount: 10, category: "Utilities" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
-    <div>
+    <>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
-    </div>
+    </>
   );
 }
 
